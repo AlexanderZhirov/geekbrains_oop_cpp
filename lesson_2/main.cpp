87404d829e1fe552828bedf931459bf0e60ebbb1
Alexander Zhirov <alexander@zhirov.website>
2021-06-21 14:22:07 +0300

    exercise 2

--- lesson_2/main.cpp
+++ lesson_2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "exercise_1.hpp"
+#include "exercise_2.hpp"
 
 using namespace std;
 
@@ -23,15 +24,25 @@ int main()
 //	students[2].printInfo();
 
 
-	vector<Student> students;
+//	vector<Student> students;
+//
+//	students.emplace_back("Олег", 20, GENDER_MALE, 75.2, 2020);
+//	students.emplace_back("Андрей", 19, GENDER_MALE, 72.8, 2020);
+//	students.emplace_back("Анастасия", 20, GENDER_FEMALE, 55.2, 2020);
+//	students.emplace_back("Ольга", 21, GENDER_FEMALE, 49.3, 2020);
+//	students.emplace_back("Владимир", 19, GENDER_MALE, 69.9, 2020);
+//
+//	Student::printCount();
 
-	students.emplace_back("Олег", 20, GENDER_MALE, 75.2, 2020);
-	students.emplace_back("Андрей", 19, GENDER_MALE, 72.8, 2020);
-	students.emplace_back("Анастасия", 20, GENDER_FEMALE, 55.2, 2020);
-	students.emplace_back("Ольга", 21, GENDER_FEMALE, 49.3, 2020);
-	students.emplace_back("Владимир", 19, GENDER_MALE, 69.9, 2020);
+	// Exercise 2
 
-	Student::printCount();
+	Apple a("red");
+	Banana b;
+	GrannySmith c;
+
+	std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+	std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+	std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
 
     return 0;
 }
