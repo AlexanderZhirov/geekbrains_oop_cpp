c3e27642b2d184c2ae46393a67604de14e73c9b4
Alexander Zhirov <alexander@zhirov.website>
2021-06-22 14:38:06 +0300

    lesson 2

--- lesson_2/main.cpp
+++ lesson_2/main.cpp
@@ -9,30 +9,19 @@ int Student::count = 0;
 
 int main()
 {
-//	Student students[] =
-//	{
-//		Student("Олег", 20, GENDER_MALE, 75.2, 2020),
-//		Student("Андрей", 19, GENDER_MALE, 72.8, 2020),
-//		Student("Анастасия", 20, GENDER_FEMALE, 55.2, 2020),
-//		Student("Ольга", 21, GENDER_FEMALE, 49.3, 2020),
-//		Student("Владимир", 19, GENDER_MALE, 69.9, 2020)
-//	};
-//
-//	Student::printCount();
-//
-//	students[0].printInfo();
-//	students[2].printInfo();
+	Student students[] =
+	{
+		Student("Олег", 20, GENDER_MALE, 75.2, 2020),
+		Student("Андрей", 19, GENDER_MALE, 72.8, 2020),
+		Student("Анастасия", 20, GENDER_FEMALE, 55.2, 2020),
+		Student("Ольга", 21, GENDER_FEMALE, 49.3, 2020),
+		Student("Владимир", 19, GENDER_MALE, 69.9, 2020)
+	};
 
+	students[0].printInfo();
+	students[2].printInfo();
 
-//	vector<Student> students;
-//
-//	students.emplace_back("Олег", 20, GENDER_MALE, 75.2, 2020);
-//	students.emplace_back("Андрей", 19, GENDER_MALE, 72.8, 2020);
-//	students.emplace_back("Анастасия", 20, GENDER_FEMALE, 55.2, 2020);
-//	students.emplace_back("Ольга", 21, GENDER_FEMALE, 49.3, 2020);
-//	students.emplace_back("Владимир", 19, GENDER_MALE, 69.9, 2020);
-//
-//	Student::printCount();
+	Student::printCount();
 
 	// Exercise 2
 
@@ -44,5 +33,53 @@ int main()
 	std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
 	std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
 
+	// Exercise 3
+	/*
+		Для реализации игры в Блэкджек необходимы данные объекты:
+
+		1) Колода карт
+		2) Игрок
+		3) Дилер
+
+		Исходя из указанных объектов можно определить несколько классов:
+
+		1) Карта
+			- номинал
+			- масть
+		2) Колода
+			- массив ссылок на объекты класса Карта
+		3) Игрок (абстрактный класс)
+			- Имя игрока
+		4) Игрок (человек, потомок от абстрактного класса Игрок)
+		5) Дилер (человек, потомок от абстрактного класса Игрок)
+		6) Рука (набор карт)
+			- массив ссылок на карты каждого игрока
+		7) Игра
+			- текущая Колода
+			- текущий Дилер
+			- текущий Игрок
+	*/
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
